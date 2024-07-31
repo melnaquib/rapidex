ebbbbfa45cfc466c96a1f4af08d19710a41aa94b
Mustafa Elnaquib <melnaquib@gmail.com>
2024-08-01 00:32:22 +0200

    Initialize example/plugin-conversations

---
+++ src/bot/index.ts
@@ -0,0 +1,75 @@
+import { autoChatAction } from '@grammyjs/auto-chat-action'
+import { hydrate } from '@grammyjs/hydrate'
+import { hydrateReply, parseMode } from '@grammyjs/parse-mode'
+import { conversations } from '@grammyjs/conversations'
+import type { BotConfig, StorageAdapter } from 'grammy'
+import { Bot as TelegramBot } from 'grammy'
+import { sequentialize } from '@grammyjs/runner'
+import { welcomeFeature } from './features/welcome.js'
+import { adminFeature } from './features/admin.js'
+import { languageFeature } from './features/language.js'
+import { unhandledFeature } from './features/unhandled.js'
+import { errorHandler } from './handlers/error.js'
+import { updateLogger } from './middlewares/update-logger.js'
+import { session } from './middlewares/session.js'
+import type { Context, SessionData } from '#root/bot/context.js'
+import { createContextConstructor } from '#root/bot/context.js'
+import { i18n, isMultipleLocales } from '#root/bot/i18n.js'
+import type { Logger } from '#root/logger.js'
+import type { Config } from '#root/config.js'
+import { greetingConversation } from '#root/bot/conversations/index.js'
+
+interface Dependencies {
+  config: Config
+  logger: Logger
+}
+
+interface Options {
+  botSessionStorage?: StorageAdapter<SessionData>
+  botConfig?: Omit<BotConfig<Context>, 'ContextConstructor'>
+}
+
+function getSessionKey(ctx: Omit<Context, 'session'>) {
+  return ctx.chat?.id.toString()
+}
+
+export function createBot(token: string, dependencies: Dependencies, options: Options = {}) {
+  const {
+    config,
+    logger,
+  } = dependencies
+
+  const bot = new TelegramBot(token, {
+    ...options.botConfig,
+    ContextConstructor: createContextConstructor({
+      logger,
+      config,
+    }),
+  })
+  const protectedBot = bot.errorBoundary(errorHandler)
+
+  // Middlewares
+  bot.api.config.use(parseMode('HTML'))
+
+  config.isPollingMode && protectedBot.use(sequentialize(getSessionKey))
+  config.isDebug && protectedBot.use(updateLogger())
+  protectedBot.use(autoChatAction(bot.api))
+  protectedBot.use(hydrateReply)
+  protectedBot.use(hydrate())
+  protectedBot.use(session({ getSessionKey, storage: options.botSessionStorage }))
+  protectedBot.use(i18n)
+  protectedBot.use(conversations())
+  protectedBot.use(greetingConversation())
+
+  // Handlers
+  protectedBot.use(welcomeFeature)
+  protectedBot.use(adminFeature)
+  isMultipleLocales && protectedBot.use(languageFeature)
+
+  // must be the last handler
+  protectedBot.use(unhandledFeature)
+
+  return bot
+}
+
+export type Bot = ReturnType<typeof createBot>
